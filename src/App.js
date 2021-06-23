6eb471b644b9af380934f9379a0c29d3588831c4
foziaShareen <foziashareen1@gmail.com>
2021-06-23 16:27:14 +0500

    useState hook

--- src/App.js
+++ src/App.js
@@ -1,31 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import axios from 'axios';
+ import React ,{Component,useState} from 'react';
+ const App=()=>{
+   const [count,setCount]=useState(0);
+  const increment=()=>{
+     setCount(count+1)
+   }
+  
+   return(
+<div>
+  <h2>counter app</h2>
+  <button onClick={increment}>clicked {count} times
 
-class App extends React.Component{
-  
-    
-    state = {
-      count: 0
-    }
-    increment=()=>{
-      this.setState({
-        count:this.state. count + 1
-      })
-    }
-    
-  
-  
-  render(){
-    return(
-      <div><h2>Counter App</h2>
-      <button onClick={ this.increment }>
-        clicked {this.state.count}times
-      </button>
-      </div>
-
-    )
-    
-  }
-}
-export default App 
+  </button>
+</div>
+   )
+   }
+ export default App
